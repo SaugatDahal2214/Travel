7cce3b91f665f12224fe5bbd45696a02205dc594
Saugat Dahal <saugat.dahal1223@gmail.com>
2023-12-07 14:20:48 +0545

    Final Commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools() 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
